c3822e31d76c7f69e385e60828a7cffbe86f8c6d
German Pogrebnyak <germanpogrebnyak14@gmail.com>
2022-09-17 15:00:05 +0300

    upd 2

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,18 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import nameUser from '.src/cli.js';
+import evenGame from '../src/brain-even.js';
 
-
-
-console.log('Welcome to the Brain Games!');
-
-console.log('May I have your name?');
-
-const name = readlineSync.question('Your answer: ');
-
-console.log(`Hello,${name}!`);
-
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-const answer1 = readlineSync.question('Your answer: ');
-
+evenGame();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,2 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!');
-
-console.log('May I have your name?');
-
-const name = readlineSync.question('Your answer: ');
-
-console.log(`Hello,${name}!`);
+import nameUser from '../src/cli.js';
